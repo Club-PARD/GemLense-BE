3dc8c94810ae072ba0a8046ddbd33ef3563ad9e6
김민제 <minje9370@handong.ac.kr>
2025-01-01 22:29:49 +0900

    modify Application add @OpenAPIDefnition

--- src/main/java/com/example/longkathon/LongkathonApplication.java
+++ src/main/java/com/example/longkathon/LongkathonApplication.java
@@ -1,7 +1,11 @@
 package com.example.longkathon;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@OpenAPIDefinition(servers = {@Server(url = "https://wecand.shop", description = "wecand")})
 
 @SpringBootApplication
 public class LongkathonApplication {
